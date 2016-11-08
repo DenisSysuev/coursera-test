c485e9b2434214b02161a3b4664047caef689a8c
DenisSysuev <denis.sysuev@gmail.com>
2016-11-08 20:11:38 +0000

    new folder

---
+++ mod1_solution/app.js
@@ -0,0 +1,33 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckContainer', MsgController);
+
+MsgController.$inject = ['$scope'];
+
+function MsgController($scope) {
+  $scope.lunchMenu = "";
+  $scope.lunchMessage = "";
+
+  $scope.checkIfTooMuch = function () {
+    var count = $scope
+            .lunchMenu.split(',')
+            .filter(n=>n.trim())
+            .filter(n=>n)
+            .length;
+    var color = "green";
+    if (count < 1) {
+      $scope.lunchMessage = "Please enter data first";
+    } else if (count < 4) {
+      $scope.lunchMessage = "Enjoy!";
+    } else {
+      $scope.lunchMessage = "Too much!";
+      color = "red";
+    }
+    $scope.lunchMessageColor = color;
+    $scope.lunchMenuColor = color;
+  };
+}
+
+})();
